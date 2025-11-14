01b745c8754625ea8f87a019e6ecf24e2c8135d6
Yael56 <yaelantonybara@gmail.com>
2025-11-14 15:30:00 -0600

    Ohol

--- ExamenIP1_YahelBarahona/src/examenip1_yahelbarahona/ExamenIP1_YahelBarahona.java
+++ ExamenIP1_YahelBarahona/src/examenip1_yahelbarahona/ExamenIP1_YahelBarahona.java
@@ -60,17 +60,77 @@ public class ExamenIP1_YahelBarahona {
                 }
      
 }
-        
+       public static void Cadenas(String cad1, String cad2) {
+       String ban = "";
+       
+           for (int i = cad1.length() - 1; i >= 0 ; i--) {
+               ban += cad1.charAt(i);
+           }
+           if (ban.equals(cad2)) {
+               System.out.println("Es una rotacion circular");}
+           else
+           System.out.println("No es una rotacion circular");
            
+       }
     
-    
+       public static boolean EsPrimo(char a){
+        return a == '3' || a == '5' || a == '7';
+       }
+       public static int Queprimo(char a){
+           int b;
+           switch (a) {
+               case '3':
+                   b = 3;
+                   break;
+               case '5':
+                   b = 5;
+                   break;
+               case '7':
+                   b = 7;
+                   break;
+               default:
+                   throw new AssertionError();
+           }
+           return b;
+       }
+       
+       public static void cifrado(String cifra, String numeros){
+         char y, z;
+         int x = 0, w = 0;
+         String primo = "", cifrado = "";
+         
+           for (int i = 0; i < numeros.length(); i++) {
+               if (EsPrimo(numeros.charAt(i))) {
+                   primo += numeros.charAt(i);
+                   y = numeros.charAt(i);
+                   x += Queprimo(y);
+               }
+               
+           }
+           if (primo.equals("")) {
+               System.out.println("No se encontraron numeros primos en la cadena. No se puede cifrar.");
+           }
+           else{
+               System.out.println("Numeros Primos Encontrados: " + primo);
+               System.out.println("Clave Generada: " + x);
+               for (int i = 0; i < cifra.length(); i++) {
+                   z = cifra.charAt(i);
+                   w = z + x;
+                   y = (char) w;
+                   cifrado += y;
+               }
+               System.out.println("Mensaje Cifrado: " + cifrado);
+           }  
+       } //15740, Clave 2451p2
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
        byte opcion;
        int num;
-        java.util.Scanner get = new java.util.Scanner(System.in);
+       String cadena1, cadena2, mensaje, num2;
+       java.util.Scanner get = new java.util.Scanner(System.in);
+        java.util.Scanner text = new java.util.Scanner(System.in);
        
         do {
             System.out.print("""
@@ -94,10 +154,21 @@ public class ExamenIP1_YahelBarahona {
                         System.out.println("Tiene que ser mayor o igual de 10");
                     break;
                 case 2:
-                    
+                    System.out.print("Cadena 1: ");
+                    cadena1 = text.nextLine();
+                    System.out.print("Cadena 2: ");
+                    cadena2 = text.nextLine();
+                    Cadenas(cadena1,cadena2);
                     break;
                 case 3:
-                    
+                    System.out.println("**Bienvenido al cifrador de mensajes**");
+                    System.out.println("Ingrese el mensaje que desea cifrar:");
+                    mensaje = text.nextLine();
+                    System.out.println("Ingrese una cadena con numeros de cifrado:");
+                    num2 = text.nextLine();
+                    System.out.println();
+                    System.out.println("\n--- Resultados ---");
+                    cifrado(mensaje, num2);
                     break;
                 case 4:
                     break;
